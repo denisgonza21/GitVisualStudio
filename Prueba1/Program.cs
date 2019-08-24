2cb136bc0a32c569c0513fb1f7e8f11055ac8e1c
denisgonza21 <denis.gonza21@gmail.com>
2019-08-23 20:13:28 -0400

    Tercer cambio

--- Prueba1/Program.cs
+++ Prueba1/Program.cs
@@ -12,6 +12,7 @@ namespace Prueba1
         {
             Console.WriteLine("Primer cambio");
             Console.WriteLine("Segundo cambio");
+            Console.WriteLine("Tercer cambio");
         }
     }
 }
